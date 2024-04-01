92b862fcafaa1ce0eb5bc2671681041a10636a63
DaniilRaptanov <daniilraptanov@gmail.com>
2024-04-01 21:38:53 +0300

    feat: addition of cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
+import cors from "cors";
 import { router } from './routes/user.routes.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
