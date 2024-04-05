7e5af8e0bdf669a181877adf835aa1d00e9211fc
Vovan <vovan.boroda2015@gmail.com>
2024-04-05 14:39:31 +0300

    feat: added functions for work with session token

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,15 +1,28 @@
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
-const secret = process.env.SECRET;
+import jwt from 'jsonwebtoken';
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+const tokenTime = process.env.TOKEN_TIME;
+const secretSessionKey = process.env.SECRET_SESSION_KEY;
 
 export function generateAccessToken(id) {
     const payload = {
         id
     };
-    return jwt.sign(payload, secret, {expiresIn: "24h"});
+    return jwt.sign(payload, secretAccessKey, {expiresIn: tokenTime});
 }
 
 export function decodeAccessToken(token) {
-    return jwt.verify(token, secret);
+    return jwt.verify(token, secretAccessKey);
+}
+
+export function generateSessionToken(id) {
+    const payload = {
+        id
+    };
+    return jwt.sign(payload, secretSessionKey, {expiresIn: tokenTime});
+}
+
+export function decodeSessionToken(token) {
+    return jwt.verify(token, secretSessionKey);
 }
