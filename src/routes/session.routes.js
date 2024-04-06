7f6a6384ab885053c2c5a18dc769cb806fea9fbc
Vovan <vovan.boroda2015@gmail.com>
2024-04-06 23:15:28 +0300

    fix: removed unnecessary line

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -2,7 +2,6 @@ import Router from 'express';
 const sessionRouter = new Router();
 import { SessionController } from '../controllers/session.controller.js';
 const sessionController = new SessionController();
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFjYzc5ZWIxLWMyNTAtNDUwNC1iOTY1LTNiZjU5OTU2MWIxNiIsImlhdCI6MTcxMjMwNjg1MywiZXhwIjoxNzEyMzI0ODUzfQ.GX82h0bb94I3SUOGuNYtjjLGWwJUPHxxvr9sAqi2Zbc";
 
 sessionRouter.post('/sessions', sessionController.createSession);
 sessionRouter.get('/sessions', sessionController.addUserOnSession);
