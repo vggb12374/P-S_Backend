d1bef52cb29f732dda0b1368a62c6aea2f84ce40
Vovan <vovan.boroda2015@gmail.com>
2024-04-18 14:20:33 +0300

    fix: some fix

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -5,7 +5,7 @@ const sessionValidMiddleware = new SessionValidMiddleware();
 import { SessionController } from '../controllers/session.controller.js';
 const sessionController = new SessionController();
 
-sessionRouter.post('/sessions/create', sessionValidMiddleware.createValidMiddleware, sessionController.createSession);
-sessionRouter.post('/sessions/connect', sessionValidMiddleware.updateValidMiddleware, sessionController.addUserOnSession);
+sessionRouter.post('/create', sessionValidMiddleware.createValidMiddleware, sessionController.createSession);
+sessionRouter.post('/connect', sessionValidMiddleware.updateValidMiddleware, sessionController.addUserOnSession);
 
 export { sessionRouter };
